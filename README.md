cacdd1ba82a69fcd7127d9e1d55dddef9b07d058
Zohaib Butt <zbutt@muvaki.com>
2019-01-09 16:49:10 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# vaultk8
-Pulls Secrets from vault and creates secrets templates to be stored in Kubernetes
+# Vaultk8
+
+This is an update to previous method... perfect case scenario is to use this in an init contianer. This will generate secrets.conf file that will be stored in a location of your choosing. Expose these secrets to your container.
+
+
+Currently supports following:
+- Vault:
+    1. Secrets Backend KV
+- k8:
+    1. Authentication against k8 backend
+    2. Generates secrets in key='value' format so they can be safely sourced for environment variables before launching app.
+
+Future:
+- LOTS of code cleanup required... this is quick rough start
+
+Python Version Support : 3.7.2
+
+## Usage
+
+The help page provides information on proper parameters to pass
+```sh                                                 
+Generates kubernetes secrets by pulling secrets from vault and generating k8
+configs
+
+positional arguments:
+  {kv}                  sub-command help
+    kv                  Pulls secrets from KV backend
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --verbose, -v         Log verbosity (default: None)
+  --no-tls-verify       Disable tls verification (default: True)
+  --vault-address VAULT_ADDRESS
+                        Enable tls verification (default:
+                        http://127.0.0.1:8200)
+  --generated-conf GENERATED_CONF, -g GENERATED_CONF
+                        Directory where the new generated secrets will be
+                        stored (stored as secrets.conf) (default: None)
+  --k8-role K8_ROLE     Role to validate against vault K8 backend (default:
+                        None)
+  --k8-auth-mount-point K8_AUTH_MOUNT_POINT
+                        Mount point for k8 auth (default: kubernetes)
+```
+
+Once the program finished it writes out **secrets.conf** to the folder specified by **generated-conf**:
+
+This program should be used in init container specifically to source secrets before launching app. Steps for ideal scenario:
+1. generate secrets in init container
+2. safe secrets to a shared mount
+3. App loads in normal lifecycle and sources secrets (environmnent variables)
+
+Currently, app only supports pulling secrets from KV (v2) backend. Sample of how to run program
+
+```sh
+python3 app/vaultk8.py --vault-address https:/vault.test.com --g /muvaki --k8-role qa-app --k8-auth-mount-point qa kv qa/app
+```
+
+**note**: if you are going to run it with docker, you will have to mount your local diretory where the k8.yaml config is sitting in order for the configs to be readable. In example above, /muvaki would be volume mounted.
---
+++ app/vaultk8.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+import os
+import hvac
+import base64
+import yaml
+import pprint
+import argparse
+from jinja2 import Environment, FileSystemLoader
+
+
+class readable_dir(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        prospective_dir=values
+        if not os.path.isdir(prospective_dir):
+            raise argparse.ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
+        if os.access(prospective_dir, os.R_OK):
+            setattr(namespace,self.dest,prospective_dir)
+        else:
+            raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
+
+def _parse_argument():
+    """ parse commandline input """
+    parser = argparse.ArgumentParser(
+        description='Generates secrets by pulling them from vault. Uses kubernetes SA to authenticate against backend',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--verbose',
+        '-v',
+        action='count',
+        help='Log verbosity')
+    parser.add_argument(
+        '--no-tls-verify',
+        action='store_false',
+        help='Disable tls verification')
+    parser.add_argument(
+        '--vault-address',
+        action='store',
+        default='http://127.0.0.1:8200',
+        help='Enable tls verification')
+    parser.add_argument(
+        '--generated-conf',
+        '-g',
+        required=True,
+        action=readable_dir,
+        help='Directory where the new generated secrets will be stored (stored as secrets.conf)')
+    parser.add_argument(
+        '--k8-role',
+        required=True,
+        action='store',
+        help='Role to validate against vault K8 backend')
+    parser.add_argument(
+        '--k8-auth-mount-point',
+        default='kubernetes',
+        action='store',
+        help='Mount point for k8 auth')
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    # Sub command that handles pulling secrets from KV backend
+    kv_parser = subparsers.add_parser('kv',
+        description='Pulls secrets from KV backend',
+        help='Pulls secrets from KV backend',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    kv_parser.add_argument(
+        '--kv-mount-point',
+        '-m',
+        default='kv',
+        type=str,
+        help='Backend KV mountpoint'
+    )
+    kv_parser.add_argument(
+        'secrets_path',
+        type=str,
+        help='Secrets Path in KV backend to pull all the secrets from'
+    )
+    kv_parser.set_defaults(func=kv)
+
+    return parser.parse_args()
+
+def vaultAuth(vault_address, tls, mount_point, role):
+    client = hvac.Client(url=vault_address) if tls \
+        else hvac.Client(url=vault_address, verify=False)
+
+    f = open('/var/run/secrets/kubernetes.io/serviceaccount/token')
+    jwt = f.read()
+    result = client.auth_kubernetes(role, jwt.strip(), mount_point=mount_point)
+    client.token = result['auth']['client_token']
+
+    assert client.is_authenticated()
+
+    return client
+
+def writeEnvConfig(secret_dir, data):
+    # Generates the secrets in key=value format
+    fh = open(secret_dir + "/secrets.conf", "w")
+    for k, v in data.items():
+        fh.write(k.strip()+'='+'\''+v.strip()+'\'\n')
+
+def kv(args):
+    # authenticating against vault
+    client =  vaultAuth(args.vault_address, args.no_tls_verify, args.k8_auth_mount_point, role=args.k8_role)
+    # setting default kv configuration
+    secret_version_response = client.secrets.kv.v2.read_secret_version(path=args.secrets_path, mount_point=args.kv_mount_point)
+    # write out the secrets
+    writeEnvConfig(args.generated_conf,secret_version_response['data']['data'])
+
+def main():
+    """ Entry point """
+    # parsing arguments
+    args = _parse_argument()
+    args.func(args)
+
+    return 0
+
+if __name__ == '__main__':
+    try:
+        exit(main())
+    except Exception as e:
+        print('Caught an unexpected exception: {}'.format(e))
+        exit(1)
