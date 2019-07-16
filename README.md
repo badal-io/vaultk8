0dfd87331ece21bd015dd5e3a7658d5139421643
Zohaib Butt <zbutt@muvaki.com>
2019-07-16 19:28:42 -0400

    added support for TOML format

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # Vaultk8
 
-This is an update to previous method... perfect case scenario is to use this in an init contianer. This will generate secrets.conf file that will be stored in a location of your choosing. Expose these secrets to your container.
+Intention of **vaultk8** is to be used as an init container to authenticate and write secrets for apps to consume in kubernetes. This app generates secret config file that will be stored in a location of your choosing.
 
 
 Currently supports following:
-- Vault:
-    1. Secrets Backend KV
-- k8:
-    1. Authentication against k8 backend
-    2. Generates secrets in key='value' format so they can be safely sourced for environment variables before launching app.
-
-Future:
-- LOTS of code cleanup required... this is quick rough start
+- Secrets backend:
+    1. Supports KV v2 secrets backend
+- Authentication:
+    1. Kubernetes Authentication backend
+- Output:
+  1. Toml: The path given in KV is traversed to generate a TOML formatted secrets file with 'section names' being the root of each of the paths
+  2. env: 'key=value' file generated
+  3. export: 'export key=value' file generated
 
 Python Version Support : 3.7.2
 
@@ -19,8 +19,8 @@ Python Version Support : 3.7.2
 
 The help page provides information on proper parameters to pass
 ```sh                                                 
-Generates kubernetes secrets by pulling secrets from vault and generating k8
-configs
+Generates secrets by pulling them from vault. Uses kubernetes SA to
+authenticate against backend
 
 positional arguments:
   {kv}                  sub-command help
@@ -28,33 +28,106 @@ positional arguments:
 
 optional arguments:
   -h, --help            show this help message and exit
-  --verbose, -v         Log verbosity (default: None)
+  --verbose, -v         Log verbosity (default: 0)
   --no-tls-verify       Disable tls verification (default: True)
   --vault-address VAULT_ADDRESS
                         Enable tls verification (default:
                         http://127.0.0.1:8200)
-  --generated-conf GENERATED_CONF, -g GENERATED_CONF
-                        Directory where the new generated secrets will be
-                        stored (stored as secrets.conf) (default: None)
+  --generated-conf-dir GENERATED_CONF_DIR, -g GENERATED_CONF_DIR
+                        Full path where the new generated secrets will be
+                        stored (default: None)
+  --generated-conf-filename GENERATED_CONF_FILENAME, -n GENERATED_CONF_FILENAME
+                        Name of the secrets file to generate (default:
+                        secrets.conf)
   --k8-role K8_ROLE     Role to validate against vault K8 backend (default:
                         None)
   --k8-auth-mount-point K8_AUTH_MOUNT_POINT
                         Mount point for k8 auth (default: kubernetes)
 ```
 
-Once the program finished it writes out **secrets.conf** to the folder specified by **generated-conf**:
-
-This program should be used in init container specifically to source secrets before launching app. Steps for ideal scenario:
-1. generate secrets in init container
-2. sace secrets to a shared mount
-3. App loads in normal lifecycle and sources secrets (environmnent variables)
-
-Currently, app only supports pulling secrets from KV (v2) backend. Sample of how to run program
-
+kv options are as follows
 ```sh
-python3 app/vaultk8.py --vault-address https:/vault.test.com --g /muvaki --k8-role qa-app --k8-auth-mount-point qa kv qa/app
+Pulls secrets from KV backend
+
+positional arguments:
+  secrets_path          Secrets Path in KV backend to pull all the secrets
+                        from
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --kv-mount-point KV_MOUNT_POINT, -m KV_MOUNT_POINT
+                        Backend KV mountpoint (default: kv)
+  --format {export,env,toml}
+                        Set config with export followed by key=value (default:
+                        env)
 ```
 
-The generated secrets are stored as secrets.conf and start with 'export key=value' format in order to make sourcing straight forward. Value is also single quoted.
+This program should be used in init container specifically to source secrets before launching app. Steps for ideal scenario:
+1. Create Kubernetes Service account as described in [vault kubernetes auth backend documentation](https://www.vaultproject.io/docs/auth/kubernetes.html)
 
-**note**: if you are going to run it with docker, you will have to mount your local diretory where the k8.yaml config is sitting in order for the configs to be readable. In example above, /muvaki would be volume mounted.
+```yaml
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: vault
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: role-tokenreview-binding
+  namespace: default
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: system:auth-delegator
+subjects:
+- kind: ServiceAccount
+  name: vault
+  namespace: default
+```
+
+2. Create a kubernetes auth backend and attach roles with appropriate policies. For example, staging cluster should hit backend that only allow staging cluster with appropriate secret access.
+3. generate secrets in init container once backend have been configured
+```yaml
+# sample init-container config for kubernetes
+# note this is not the full deployment spec
+...
+spec:
+  serviceAccountName: vault
+  initContainers:
+    - name: vaultk8
+      image: muvaki/vaultk8
+      args:
+        - "--vault-address"
+        - "https://vault.com"
+        - "-g"
+        - "/etc/vault"
+        - "-n"
+        - "env"
+        - "--k8-role"
+        - "vault"
+        - "--k8-auth-mount-point"
+        - "kubernetes"
+        - "kv"
+        - "staging/my_app"
+...
+```
+4. save secrets to a shared volume mount so your apps can consume it. Make it memory to be volatile and not persistant
+```yaml
+# sample kubernetes deployment spec
+volumes:
+        - name: secrets-vol
+          emptyDir:
+            medium: "Memory"
+```
+5. App loads in normal lifecycle and sources secrets (environmnent variables)
+```yaml
+     containers:
+      - name: muvaki
+        image: muvaki-web
+        command:
+          - "bash"
+          - "-c"
+          - ". /etc/vault/env && node hello.js"
+```
--- app/vaultk8.py
+++ app/vaultk8.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
-import os
-import hvac
-import base64
-import yaml
-import pprint
-import argparse
-from jinja2 import Environment, FileSystemLoader
+"""
+vaultk8 generates secret files from paths in KV. Supports 3 formats:
+- key=value
+- export key=value
+- TOML: builds of the headings in the root of KV store
+"""
 
+import os
+import sys
+import hvac
+import yaml
+import toml
+import argparse
+import logging
+
+log = logging.getLogger('vaultk8')
 
 class readable_dir(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -28,6 +36,7 @@ def _parse_argument():
         '--verbose',
         '-v',
         action='count',
+        default=0,
         help='Log verbosity')
     parser.add_argument(
         '--no-tls-verify',
@@ -39,11 +48,17 @@ def _parse_argument():
         default='http://127.0.0.1:8200',
         help='Enable tls verification')
     parser.add_argument(
-        '--generated-conf',
+        '--generated-conf-dir',
         '-g',
         required=True,
         action=readable_dir,
-        help='Directory where the new generated secrets will be stored (stored as secrets.conf)')
+        help='Full path where the new generated secrets will be stored')
+    parser.add_argument(
+        '--generated-conf-filename',
+        '-n',
+        type=str,
+        default='secrets.conf',
+        help='Name of the secrets file to generate')
     parser.add_argument(
         '--k8-role',
         required=True,
@@ -69,19 +84,30 @@ def _parse_argument():
         help='Backend KV mountpoint'
     )
     kv_parser.add_argument(
+        '--format',
+        type=str,
+        choices=['export', 'env', 'toml'],
+        help='Set config with export followed by key=value',
+        default='env')
+    kv_parser.set_defaults(func=kv)
+    kv_parser.add_argument(
         'secrets_path',
         type=str,
         help='Secrets Path in KV backend to pull all the secrets from'
     )
-    kv_parser.add_argument(
-        '--export',
-        action='store_true',
-        help='Set config with export followed by key=value')
-    kv_parser.set_defaults(func=kv)
+    
+    arguments = parser.parse_args()
 
-    return parser.parse_args()
+    # set log level
+    log.setLevel(max(3 - arguments.verbose, 1) * 10)
+    return arguments
+
+def Merge(dict1, dict2): 
+    res = {**dict1, **dict2} 
+    return res 
 
 def vaultAuth(vault_address, tls, mount_point, role):
+    log.info("Authenticating via K8 backend")
     client = hvac.Client(url=vault_address) if tls \
         else hvac.Client(url=vault_address, verify=False)
 
@@ -94,34 +120,83 @@ def vaultAuth(vault_address, tls, mount_point, role):
 
     return client
 
-def writeEnvConfig(secret_dir, data, export_setting):
-    # Generates the secrets in key=value format
-    fh = open(secret_dir + "/secrets.conf", "w")
-    for k, v in data.items():
-        if export_setting:
-            fh.write('export ' + k.strip()+'='+'\''+v.strip()+'\'\n')
+def writeEnvConfig(filename, data, file_format):
+    log.info("writing secrets to " + filename)
+    # Generates the secrets in key=value or toml
+    fh = open(filename, "w")
+    if file_format == 'toml':
+        fh.write(toml.dumps(data))
+    else:
+        for k, v in data.items():
+            if file_format == 'export':
+                fh.write('export ' + k.strip()+'='+'\''+v.strip()+'\'\n')
+            else:
+                fh.write(k.strip()+'='+'\''+v.strip()+'\'\n')
+    fh.close()
+
+def readKVSecrets(client, secrets_path, mount_point, value_type='secrets'):
+    secret = {}
+    try:
+        if value_type == 'secrets':
+            secret = client.secrets.kv.v2.read_secret_version(path=secrets_path, mount_point=mount_point)['data']['data']
         else:
-            fh.write(k.strip()+'='+'\''+v.strip()+'\'\n')
+            secret = client.secrets.kv.v2.list_secrets(path=secrets_path, mount_point=mount_point)['data']
+    except None:
+        pass
+    finally:
+        return secret
+
+def getTOMLFormat(client, path, mount):
+    tmp_toml = {}
+    # Reads any keys stored in root of path and adds it to the toml dict
+    toml = readKVSecrets(client, path, mount)
+
+    # recursively traverses the tree to generate keys and get secrets for kv backend
+    # selection names are considered the root of the path in KV with key/value inside.
+    # any key/value that fall under the path are added under the subsequent selection name.
+    kv_keys = readKVSecrets(client, path, mount, 'key')
+    if 'keys' in kv_keys:
+        for k in kv_keys['keys']:
+            if k.endswith('/'):
+                # this is to prevent same keys being traversed twice
+                if k[:-1] in kv_keys['keys']:
+                    continue
+                else:
+                    tmp_toml[k[:-1]] = getTOMLFormat(client, path + '/' + k[:-1], mount)
+            else:
+                tmp_toml[k] = getTOMLFormat(client, path + '/' + k, mount)
+        toml = Merge(tmp_toml, toml)
+    return toml
 
 def kv(args):
+    results = {}
+    log.info("grabbing secrets from KV")
     # authenticating against vault
     client =  vaultAuth(args.vault_address, args.no_tls_verify, args.k8_auth_mount_point, role=args.k8_role)
     # setting default kv configuration
-    secret_version_response = client.secrets.kv.v2.read_secret_version(path=args.secrets_path, mount_point=args.kv_mount_point)
+    if args.format == 'toml':
+        log.info("generating TOML formatted secrets")
+        results = getTOMLFormat(client, args.secrets_path, args.kv_mount_point)
+    else:
+        log.info("generating K/V formatted secrets")
+        results = readKVSecrets(client, args.secrets_path, args.kv_mount_point)
+
     # write out the secrets
-    writeEnvConfig(args.generated_conf,secret_version_response['data']['data'], args.export)
+    filename = args.generated_conf_dir + '/' + args.generated_conf_filename
+    writeEnvConfig(filename, results, args.format)
 
 def main():
     """ Entry point """
+    # sets up debug logging and logger format
+    logging.basicConfig(format='%(name)s (%(levelname)s): %(message)s')
     # parsing arguments
-    args = _parse_argument()
-    args.func(args)
-
-    return 0
+    try:
+        args = _parse_argument()
+        args.func(args)
+    except Exception as e:
+        log.error('Caught an unexpected exception: {}'.format(e))
+    finally:
+        logging.shutdown()
 
 if __name__ == '__main__':
-    try:
-        exit(main())
-    except Exception as e:
-        print('Caught an unexpected exception: {}'.format(e))
-        exit(1)
+    sys.exit(main())
